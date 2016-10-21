070c3ce157132a160ae96db6a4878e711b03381d
samcheck <samcheckster@gmail.com>
2016-10-21 17:44:43 -0500

    logging to file

--- app/__init__.py
+++ app/__init__.py
@@ -21,11 +21,10 @@ from app import views, models
 
 if not app.debug:
     import logging
-    from logging.handlers import SMTPHandler
-    credentials = None
-    if MAIL_USERNAME or MAIL_PASSWORD:
-        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
-    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER,
-                                ADMINS, 'picoblog failure', credentials)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/picoblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.loger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('picoblog startup')
