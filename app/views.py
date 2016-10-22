5e6379c5f2785097dbe6f1b4d5c64cf98ebef819
samcheck <samcheckster@gmail.com>
2016-10-22 15:21:32 -0500

    making users follow themselves

--- app/views.py
+++ app/views.py
@@ -72,6 +72,9 @@ def after_login(resp):
         user = User(nickname=nickname, email=resp.email)
         db.session.add(user)
         db.session.commit()
+        # make the user follow themself
+        db.session.add(user.follow(user))
+        db.session.commit()
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
