9c3f2cfab17c0c6826e2af2952bd8dd61f7e7409
samcheck <samcheckster@gmail.com>
2016-11-30 17:09:12 -0600

    updating with DB profiling

--- config.py
+++ config.py
@@ -16,6 +16,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False # explictly set to suppress warnings
+SQLALCHEMY_RECORD_QUERIES = True
+# slow db query threshold (secs)
+DATABASE_QUERY_TIMEOUT = 0.5
 
 # Flask-WhooshAlchemy search db
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
