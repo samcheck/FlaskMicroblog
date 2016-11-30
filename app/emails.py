4c52ae1066cacd00e9402c388c46be58fb3a371b
samcheck <samcheckster@gmail.com>
2016-11-29 21:02:55 -0600

    adding/converting to async decorator

--- app/emails.py
+++ app/emails.py
@@ -1,20 +1,20 @@
-from threading import Thread
 from flask import render_template
 from flask_mail import Message
 from app import app, mail
 from config import ADMINS
+from .decorators import async
 
+@async
 def send_async_email(app, msg):
     with app.app_context():
         mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
-    mail_thread = Thread(target=send_async_email, args=[app, msg])
-    mail_thread.start()
+    send_async_email(app, msg)
 
 
 def follower_notification(followed, follower):
