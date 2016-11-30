1c83cc39fe86b0996a1e0ce89f29e2bb132f30e1
samcheck <samcheckster@gmail.com>
2016-11-29 20:55:38 -0600

    add async email call

--- app/emails.py
+++ app/emails.py
@@ -1,12 +1,20 @@
+from threading import Thread
 from flask import render_template
 from flask_mail import Message
-from app import mail
+from app import app, mail
 from config import ADMINS
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg.body = text_body
     msg.html = html_body
     mail.send(msg)
+    mail_thread = Thread(target=send_async_email, args=[app, msg])
+    mail_thread.start()
 
 
 def follower_notification(followed, follower):
