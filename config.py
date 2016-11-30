fc49023d4b07bd89245865eef30c8966cbef0031
samcheck <samcheckster@gmail.com>
2016-11-29 20:15:51 -0600

    update config for production mail server

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
@@ -8,7 +10,6 @@ OPENID_PROVIDERS = [
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
 
-import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # SQLalchemy settings
@@ -19,16 +20,26 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False # explictly set to suppress warnings
 # Flask-WhooshAlchemy search db
 WHOOSH_BASE = os.path.join(basedir, 'search.db')
 
-# mail server settings
-MAIL_SERVER = 'localhost'
-MAIL_PORT = 25
-MAIL_USERNAME = None
-MAIL_PASSWORD = None
+# debug mail server settings
+# MAIL_SERVER = 'localhost'
+# MAIL_PORT = 25
+# MAIL_USERNAME = None
+# MAIL_PASSWORD = None
 
-# admin list
-ADMINS = ['you@example.com']
 # command to run the above debugging email server locally
 # python3 -m smtpd -n -c DebuggingServer localhost:25
+
+# production mail server settings
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+# admin list
+ADMINS = os.environ.get('MAIL_USERNAME') # using as default
+
 
 
 #pagination
